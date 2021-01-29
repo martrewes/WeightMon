c96ec9d80c0871454f2cf9060725f8ce5d99ec38
martrewes <martinweston90@gmail.com>
2021-01-29 12:21:20 +0000

    Added radio for bot people, added new grid view, used objects to define adding data

--- WeightMon/Form1.Designer.vb
+++ WeightMon/Form1.Designer.vb
@@ -34,7 +34,17 @@ Partial Class Main
         Me.tbxLbs = New System.Windows.Forms.TextBox()
         Me.RadioButton1 = New System.Windows.Forms.RadioButton()
         Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.grdSio = New System.Windows.Forms.DataGridView()
+        Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.radBen = New System.Windows.Forms.RadioButton()
+        Me.radSio = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         CType(Me.grdBen, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.grdSio, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'grdBen
@@ -43,12 +53,13 @@ Partial Class Main
         Me.grdBen.AllowUserToDeleteRows = False
         Me.grdBen.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.grdBen.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.colDate, Me.colWeight, Me.colLbs, Me.colStone})
-        Me.grdBen.Location = New System.Drawing.Point(42, 267)
+        Me.grdBen.Location = New System.Drawing.Point(20, 272)
+        Me.grdBen.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.grdBen.Name = "grdBen"
         Me.grdBen.RowHeadersVisible = False
         Me.grdBen.RowHeadersWidth = 62
         Me.grdBen.RowTemplate.Height = 28
-        Me.grdBen.Size = New System.Drawing.Size(676, 150)
+        Me.grdBen.Size = New System.Drawing.Size(254, 165)
         Me.grdBen.TabIndex = 0
         '
         'colDate
@@ -81,9 +92,10 @@ Partial Class Main
         '
         'btnAdd
         '
-        Me.btnAdd.Location = New System.Drawing.Point(108, 87)
+        Me.btnAdd.Location = New System.Drawing.Point(455, 225)
+        Me.btnAdd.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnAdd.Name = "btnAdd"
-        Me.btnAdd.Size = New System.Drawing.Size(116, 50)
+        Me.btnAdd.Size = New System.Drawing.Size(77, 32)
         Me.btnAdd.TabIndex = 1
         Me.btnAdd.Text = "Add"
         Me.btnAdd.UseVisualStyleBackColor = True
@@ -91,38 +103,43 @@ Partial Class Main
         'DateTimePicker1
         '
         Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
-        Me.DateTimePicker1.Location = New System.Drawing.Point(283, 69)
+        Me.DateTimePicker1.Location = New System.Drawing.Point(208, 19)
+        Me.DateTimePicker1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.DateTimePicker1.Name = "DateTimePicker1"
-        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 26)
+        Me.DateTimePicker1.Size = New System.Drawing.Size(98, 20)
         Me.DateTimePicker1.TabIndex = 2
         '
         'tbxKG
         '
-        Me.tbxKG.Location = New System.Drawing.Point(283, 157)
+        Me.tbxKG.Location = New System.Drawing.Point(20, 237)
+        Me.tbxKG.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.tbxKG.Name = "tbxKG"
-        Me.tbxKG.Size = New System.Drawing.Size(100, 26)
+        Me.tbxKG.Size = New System.Drawing.Size(68, 20)
         Me.tbxKG.TabIndex = 3
         '
         'tbxStone
         '
-        Me.tbxStone.Location = New System.Drawing.Point(451, 157)
+        Me.tbxStone.Location = New System.Drawing.Point(132, 237)
+        Me.tbxStone.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.tbxStone.Name = "tbxStone"
-        Me.tbxStone.Size = New System.Drawing.Size(100, 26)
+        Me.tbxStone.Size = New System.Drawing.Size(68, 20)
         Me.tbxStone.TabIndex = 4
         '
         'tbxLbs
         '
-        Me.tbxLbs.Location = New System.Drawing.Point(570, 157)
+        Me.tbxLbs.Location = New System.Drawing.Point(211, 237)
+        Me.tbxLbs.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.tbxLbs.Name = "tbxLbs"
-        Me.tbxLbs.Size = New System.Drawing.Size(95, 26)
+        Me.tbxLbs.Size = New System.Drawing.Size(65, 20)
         Me.tbxLbs.TabIndex = 5
         '
         'RadioButton1
         '
         Me.RadioButton1.AutoSize = True
-        Me.RadioButton1.Location = New System.Drawing.Point(283, 112)
+        Me.RadioButton1.Location = New System.Drawing.Point(20, 208)
+        Me.RadioButton1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.RadioButton1.Name = "RadioButton1"
-        Me.RadioButton1.Size = New System.Drawing.Size(65, 24)
+        Me.RadioButton1.Size = New System.Drawing.Size(45, 17)
         Me.RadioButton1.TabIndex = 6
         Me.RadioButton1.TabStop = True
         Me.RadioButton1.Text = "KGs"
@@ -131,30 +148,113 @@ Partial Class Main
         'RadioButton2
         '
         Me.RadioButton2.AutoSize = True
-        Me.RadioButton2.Location = New System.Drawing.Point(451, 112)
+        Me.RadioButton2.Location = New System.Drawing.Point(132, 208)
+        Me.RadioButton2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.RadioButton2.Name = "RadioButton2"
-        Me.RadioButton2.Size = New System.Drawing.Size(80, 24)
+        Me.RadioButton2.Size = New System.Drawing.Size(55, 17)
         Me.RadioButton2.TabIndex = 7
         Me.RadioButton2.TabStop = True
         Me.RadioButton2.Text = "St Lbs"
         Me.RadioButton2.UseVisualStyleBackColor = True
         '
+        'grdSio
+        '
+        Me.grdSio.AllowUserToAddRows = False
+        Me.grdSio.AllowUserToDeleteRows = False
+        Me.grdSio.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grdSio.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn2, Me.DataGridViewTextBoxColumn3, Me.DataGridViewTextBoxColumn4})
+        Me.grdSio.Location = New System.Drawing.Point(278, 272)
+        Me.grdSio.Margin = New System.Windows.Forms.Padding(2)
+        Me.grdSio.Name = "grdSio"
+        Me.grdSio.RowHeadersVisible = False
+        Me.grdSio.RowHeadersWidth = 62
+        Me.grdSio.RowTemplate.Height = 28
+        Me.grdSio.Size = New System.Drawing.Size(254, 165)
+        Me.grdSio.TabIndex = 8
+        '
+        'DataGridViewTextBoxColumn1
+        '
+        Me.DataGridViewTextBoxColumn1.HeaderText = "Date"
+        Me.DataGridViewTextBoxColumn1.MinimumWidth = 8
+        Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
+        Me.DataGridViewTextBoxColumn1.Width = 150
+        '
+        'DataGridViewTextBoxColumn2
+        '
+        Me.DataGridViewTextBoxColumn2.HeaderText = "Weight"
+        Me.DataGridViewTextBoxColumn2.MinimumWidth = 8
+        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
+        Me.DataGridViewTextBoxColumn2.Width = 150
+        '
+        'DataGridViewTextBoxColumn3
+        '
+        Me.DataGridViewTextBoxColumn3.HeaderText = "Lbs"
+        Me.DataGridViewTextBoxColumn3.MinimumWidth = 8
+        Me.DataGridViewTextBoxColumn3.Name = "DataGridViewTextBoxColumn3"
+        Me.DataGridViewTextBoxColumn3.Width = 150
+        '
+        'DataGridViewTextBoxColumn4
+        '
+        Me.DataGridViewTextBoxColumn4.HeaderText = "Stone"
+        Me.DataGridViewTextBoxColumn4.MinimumWidth = 8
+        Me.DataGridViewTextBoxColumn4.Name = "DataGridViewTextBoxColumn4"
+        Me.DataGridViewTextBoxColumn4.Width = 150
+        '
+        'radBen
+        '
+        Me.radBen.AutoSize = True
+        Me.radBen.Location = New System.Drawing.Point(6, 19)
+        Me.radBen.Name = "radBen"
+        Me.radBen.Size = New System.Drawing.Size(44, 17)
+        Me.radBen.TabIndex = 9
+        Me.radBen.TabStop = True
+        Me.radBen.Text = "Ben"
+        Me.radBen.UseVisualStyleBackColor = True
+        '
+        'radSio
+        '
+        Me.radSio.AutoSize = True
+        Me.radSio.Location = New System.Drawing.Point(56, 19)
+        Me.radSio.Name = "radSio"
+        Me.radSio.Size = New System.Drawing.Size(64, 17)
+        Me.radSio.TabIndex = 10
+        Me.radSio.TabStop = True
+        Me.radSio.Text = "Siobhan"
+        Me.radSio.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.radSio)
+        Me.GroupBox1.Controls.Add(Me.radBen)
+        Me.GroupBox1.Controls.Add(Me.DateTimePicker1)
+        Me.GroupBox1.Location = New System.Drawing.Point(20, 133)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(311, 55)
+        Me.GroupBox1.TabIndex = 11
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "User and Date"
+        '
         'Main
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(759, 544)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.grdSio)
         Me.Controls.Add(Me.RadioButton2)
         Me.Controls.Add(Me.RadioButton1)
         Me.Controls.Add(Me.tbxLbs)
         Me.Controls.Add(Me.tbxStone)
         Me.Controls.Add(Me.tbxKG)
-        Me.Controls.Add(Me.DateTimePicker1)
         Me.Controls.Add(Me.btnAdd)
         Me.Controls.Add(Me.grdBen)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "Main"
         Me.Text = "Form1"
         CType(Me.grdBen, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.grdSio, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -172,4 +272,12 @@ Partial Class Main
     Friend WithEvents tbxLbs As TextBox
     Friend WithEvents RadioButton1 As RadioButton
     Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents grdSio As DataGridView
+    Friend WithEvents DataGridViewTextBoxColumn1 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn2 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn3 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn4 As DataGridViewTextBoxColumn
+    Friend WithEvents radBen As RadioButton
+    Friend WithEvents radSio As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
 End Class
--- WeightMon/Form1.vb
+++ WeightMon/Form1.vb
@@ -1,44 +1,24 @@
 ﻿Public Class Main
-    Private Sub ListView1_SelectedIndexChanged(sender As Object, e As EventArgs)
 
-    End Sub
+    Dim strDate As Date
+    Dim WeightKG As Decimal
+    Dim WeightSt As Integer
+    Dim WeightStLbs As String
+    Dim WeightLbs As Decimal
+    Dim GridView As Object
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
-        Dim strDate As Date
-        Dim WeightKG As Decimal
-        Dim WeightSt As Integer
-        Dim WeightStLbs As String
-        Dim WeightLbs As Decimal
 
 
+        If radBen.Checked = True Then
+            GridView = grdBen
+        ElseIf radSio.Checked = True Then
+            GridView = grdSio
+        End If
 
-
-        If RadioButton1.Checked = True Then
-            strDate = DateTimePicker1.Value
-            WeightKG = tbxKG.Text
-            WeightLbs = Math.Round(WeightKG * 2.20462, 2)
-            WeightSt = WeightLbs \ 14
-            WeightStLbs = WeightSt & "st, " & WeightLbs - (WeightSt * 14) & "lbs"
-            grdBen.Rows.Add(strDate.ToShortDateString, WeightKG, WeightLbs, WeightStLbs)
-
-            grdBen.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells
-            grdBen.AutoResizeColumns()
-            MsgBox(strDate.ToShortDateString & "    KG: " & WeightKG & ", Lbs: " & WeightLbs & ", And " & WeightStLbs)
-
-
-        ElseIf RadioButton2.Checked = True Then
-
-            strDate = DateTimePicker1.Value
-
-            WeightLbs = (tbxStone.Text * 14) + tbxLbs.Text
-            WeightSt = tbxStone.Text
-            WeightKG = Math.Round(WeightLbs / 2.20462, 2)
-            WeightStLbs = tbxStone.Text & "st, " & tbxLbs.Text & "lbs"
-            grdBen.Rows.Add(strDate.ToShortDateString, WeightKG, WeightLbs, WeightStLbs)
-            MsgBox(strDate.ToShortDateString & "    KG: " & WeightKG & ", Lbs: " & WeightLbs & ", And " & WeightStLbs)
-
-        Else
-            MsgBox("Please select a Weight unit")
+        If GridView Is Nothing Then
+            MsgBox("Select user")
+        Else AddData()
         End If
 
     End Sub
@@ -53,5 +33,45 @@
         tbxLbs.Enabled = False
         tbxStone.Enabled = False
         tbxKG.Enabled = True
+    End Sub
+
+    Private Sub AddData()
+        If RadioButton1.Checked = True Then
+
+
+
+            strDate = DateTimePicker1.Value
+            WeightKG = tbxKG.Text
+            WeightLbs = Math.Round(WeightKG * 2.20462, 2)
+            WeightSt = WeightLbs \ 14
+            WeightStLbs = WeightSt & "st, " & WeightLbs - (WeightSt * 14) & "lbs"
+            GridView.Rows.Add(strDate.ToShortDateString, WeightKG, WeightLbs, WeightStLbs)
+
+            GridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells
+            GridView.AutoResizeColumns()
+            MsgBox(strDate.ToShortDateString & "    KG: " & WeightKG & ", Lbs: " & WeightLbs & ", And " & WeightStLbs)
+
+
+        ElseIf RadioButton2.Checked = True Then
+
+            strDate = DateTimePicker1.Value
+
+            WeightLbs = (tbxStone.Text * 14) + tbxLbs.Text
+            WeightSt = tbxStone.Text
+            WeightKG = Math.Round(WeightLbs / 2.20462, 2)
+            WeightStLbs = tbxStone.Text & "st, " & tbxLbs.Text & "lbs"
+            GridView.Rows.Add(strDate.ToShortDateString, WeightKG, WeightLbs, WeightStLbs)
+            GridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells
+            GridView.AutoResizeColumns()
+            MsgBox(strDate.ToShortDateString & "    KG: " & WeightKG & ", Lbs: " & WeightLbs & ", And " & WeightStLbs)
+
+        Else
+            MsgBox("Please select a Weight unit")
+        End If
+
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles radBen.CheckedChanged
+
     End Sub
 End Class
